78f65bb9743e8a1094cf8786f7acad1080e8b971
Areen Chakraborty <62027600+04ac@users.noreply.github.com>
2025-01-20 01:44:14 +0530

    Update dependencies and configurations

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from youtube_transcript_api import YouTubeTranscriptApi
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from dotenv import load_dotenv
 import os
-import json
-import requests
 
 
+load_dotenv()
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+def root():
     return {
         "message": "Use endpoint /transcript/{video_id}",
         "docs": "To access docs, use endpoint /docs",
@@ -19,21 +19,24 @@ async def root():
 @app.get("/transcript/{video_id}")
 def get_transcript_from_video_id(video_id: str):
     # video_id = "Z6nkEZyS9nA"
-    transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
-    complete_transcript = ""
-    for transcript in transcript_list:
-        text = transcript["text"]
-        complete_transcript += text + " "
+    try:
+        transcript_list = YouTubeTranscriptApi.get_transcript(video_id, proxies={"http": os.getenv("PROXY_URL")})
+        complete_transcript = ""
+        for transcript in transcript_list:
+            text = transcript["text"]
+            complete_transcript += text + " "
 
-        # Uncomment this if you want the transcript in a file
-        # with open(f"transcript_{video_id}.txt", "a") as opf:
-        #     opf.write(text + "\n")
+            # Uncomment this if you want the transcript in a file
+            # with open(f"transcript_{video_id}.txt", "a") as opf:
+            #     opf.write(text + "\n")
 
     # Ensure transcript only contains utf-8 chars
-    complete_transcript = complete_transcript.encode("utf-8", errors="ignore").decode(
-        "utf-8"
-    )
-    return {"transcript": complete_transcript}
+        complete_transcript = complete_transcript.encode("utf-8", errors="ignore").decode(
+            "utf-8"
+        )
+        return {"transcript": complete_transcript}
+    except Exception as e:
+        return {"error": str(e), "url": os.getenv("PROXY_URL")}
 
 # To run
 # ```uvicorn app:app --reload```
